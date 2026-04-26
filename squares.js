d3af4feda78109b3cb77a20e5cea4c5a24f4a1f8
alexrivas05 <alex12rivas34@gmail.com>
2026-04-25 21:44:07 -0400

    Updated Loop Using addSquare()

--- squares.js
+++ squares.js
@@ -3,14 +3,7 @@ let squareArea = document.querySelector("#squarearea");
 let squareCount = parseInt(Math.random()*21)+30; //30-50
 //make some variable for really big index
 for(let i = 0; i<squareCount;i++){
-    let square = document.createElement("div");
-    square.className = "square";
-    square.style.left = parseInt(Math.random()*650)+"px";
-    square.style.top = parseInt(Math.random()*250)+"px";
-    square.style.backgroundColor = getRandomColor();
-    //I can put an onclick on a function(this)
-    //put it on the screen
-    squareArea.append(square);
+    addSquare();
 }
 
 function getRandomColor(){
